bab579f8f58ae1cbec9c0108df4c7b189f31edc0
KhoaDoAntiCode <khoado0123@gmail.com>
2025-02-17 11:15:32 +0700

    Add Views for CRUD

--- Net1728Group2MVC/Controllers/NewsTagController.cs
+++ Net1728Group2MVC/Controllers/NewsTagController.cs
@@ -1,19 +1,86 @@
-﻿using BLL.Interfaces;
-using BLL.Services;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using BLL.Interfaces;
 using DAL.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Net1728Group2MVC.Controllers
 {
-    public class NewsTagController : Controller
+    public class TagController : Controller
     {
         private readonly ITagService _tagService;
-        public NewsTagController(ITagService tagService)
+
+        public TagController(ITagService newsTagService)
         {
-            _tagService = tagService;
+            _tagService = newsTagService;
         }
 
-        public IList<Tag> Tag { get; set; } = default!;
+        public async Task<IActionResult> Index()
+        {
+            var newsTags =  _tagService.GetTags();
+            return View("~/Views/Tag/Index.cshtml", newsTags);
+        }
+
+        public IActionResult Create()
+        {
+            return View("~/Views/Tag/Create.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Tag newsTag)
+        {
+            if (ModelState.IsValid)
+            {
+                 _tagService.CreateTag(newsTag);
+                return RedirectToAction(nameof(Index));
+            }
+            return View($"~/Views/Tag/{nameof(newsTag)}.cshtml", newsTag);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var newsTag = await _tagService.GetTags().FirstOrDefault(t => t.TagId == id);
+   
+            if (newsTag == null)
+            {
+                return NotFound();
+            }
+            return View($"~/Views/Tag/{nameof(newsTag)}.cshtml", newsTag);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Tag newsTag)
+        {
+            if (id != newsTag.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _tagService.UpdateTag(newsTag);
+                return RedirectToAction(nameof(Index));
+            }
+            return View($"~/Views/Tag/{nameof(newsTag)}.cshtml", newsTag);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var newsTag = await _tagService.DeleteTag(id);
+            if (newsTag == null)
+            {
+                return NotFound();
+            }
+            return View($"~/Views/Tag/{nameof(newsTag)}.cshtml", newsTag);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _tagService.DeleteTag(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
