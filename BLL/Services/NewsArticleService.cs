a63f3cddf9c3d4c4676c0b318421122cc5831516
NET_12_NguyenQuangKhanhAn <annqkhe180905@fpt.edu.vn>
2025-02-17 09:40:28 +0700

    save before merge

---
+++ BLL/Services/NewsArticleService.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using DAL.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class NewsArticleService : INewsArticleRepository
+    {
+        private readonly INewsArticleRepository _newsArticleRepository;
+        private readonly ILogger
+        
+        public NewsArticleService (NewsArticleRepository newsArticleRepository)
+        {
+            newsArticleRepository = (NewsArticleRepository)_newsArticleRepository;
+        }
+
+        public Task<NewsArticle> CreateNewsArticle(NewsArticle news)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsArticle> DeleteNewsArticle(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<NewsArticle>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsArticle> GetNewsArticleByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsArticle> UpdateNewsArticle(NewsArticle news)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Interfaces/INewsArticleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface INewsArticleRepository
+    {
+        Task<IEnumerable<NewsArticle>> GetAllAsync();
+        Task<NewsArticle> GetNewsArticleByIdAsync(string id);
+        Task <NewsArticle> CreateNewsArticle(NewsArticle news);
+        Task <NewsArticle> UpdateNewsArticle(NewsArticle news);
+        Task <NewsArticle> DeleteNewsArticle(string id);
+    }
+}
